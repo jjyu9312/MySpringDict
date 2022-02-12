43c3859a4aec23a2629fbf696c9b35b5f3764ca7
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-02-12 18:55:57 +0900

    Initial commit

---
+++ src/test/java/com/example/myspringdict/MySpringDictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringdict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringDictApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
